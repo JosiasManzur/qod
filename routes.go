7269ef566900a5cc49285fe075c9154e0c90e1ca
JosiasManzur <157073926+JosiasManzur@users.noreply.github.com>
2025-08-20 19:55:25 -0600

    Create routes.go

---
+++ routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// routes specifies our routes (routes.go)
+func (app *application) routes() http.Handler {
+   router := httprouter.New()
+   router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+   return router
+}
